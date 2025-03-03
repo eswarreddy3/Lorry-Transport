158b259a31b7575c2aff8ab57fa29a83c656fe0f
Venkata Sai Eswar Reddy Pagadala <109944287+eswarreddy3@users.noreply.github.com>
2025-03-03 12:11:07 +0530

    Update Contact.jsx

--- lorry-ui/src/Pages/Contact.jsx
+++ lorry-ui/src/Pages/Contact.jsx
@@ -22,7 +22,7 @@ function Contact() {
               <p className="paraContact" style={{ marginLeft: '50px' }}> K.Siva Rama Krishna Reddy</p>
             </section>
             <section className="ownerInfo">
-              <p className="paraContact"><FontAwesomeIcon className="icon" icon={faPhone} /> Phone:<a href="tel:+919347357455"> <span style={{color:'wheat'}}>+91 9347357455</span></a></p>
+              <p className="paraContact"><FontAwesomeIcon className="icon" icon={faPhone} /> Phone:<a href="tel:+919347357455"> <span style={{color:'black'}}>+91 9347357455</span></a></p>
             </section>
             <section className="ownerInfo">
               <p className="paraContact"><FontAwesomeIcon className="icon" icon={faMapMarkerAlt} /> Near Kallam College, Guntur-Chennai Hwy, Dasaripalem, Andhra Pradesh 522019 India</p>
@@ -33,7 +33,7 @@ function Contact() {
           </div>
           <div className="mapArea">
             <a href="https://maps.app.goo.gl/YtzkGZYFKFQ9Nj7U6" target="_blank">
-              <h2 style={{color:'wheat'}}>Locate Us <FontAwesomeIcon icon={faMapMarkerAlt} /></h2>
+              <h2 style={{color:'black'}}>Locate Us <FontAwesomeIcon icon={faMapMarkerAlt} /></h2>
               <iframe src="https://www.google.com/maps/embed?pb=!4v1739268517796!6m8!1m7!1sWQ3hWh8ByLLELsE4rfWoHw!2m2!1d16.25554373876932!2d80.33681281163777!3f195.7448045944573!4f6.025420394054393!5f0.7820865974627469"
                 style={{ width: '100%', height: '100%', border: '0' }}
                 loading="lazy"
