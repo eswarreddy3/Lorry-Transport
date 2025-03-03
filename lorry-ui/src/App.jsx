5155fb909d5e3e7bf3ba46dbda565973cc23161d
Venkata Sai Eswar Reddy Pagadala <109944287+eswarreddy3@users.noreply.github.com>
2025-03-03 11:59:59 +0530

    Update App.jsx

--- lorry-ui/src/App.jsx
+++ lorry-ui/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { BrowserRouter, Link, Route, Routes, } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Link from 'react-router-dom'
 import Bookinghistory from './Pages/Bookinghistory'
 import Booking from './Pages/Booking'
 import Navigation from './components/Navigation'
@@ -19,23 +20,18 @@ function App() {
   return (
     <>
     
-      <BrowserRouter>
-        <Navigation/>
-          {/* <PDFViewer style={{ width: '100%', height: 800 }}>
-            <MyDocument />
-          </PDFViewer> */}
-        {/* <Link to="/booking">Booking</Link>
-        <Link to="/bookinghistory">Booking History</Link> */}
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/about' element={<About/>}></Route>
-          <Route path='/contact' element={<Contact/>}></Route>
-          <Route path='/booking' element={<ProtectedRoute><Booking /></ProtectedRoute>}></Route>
-          <Route path='/bookinghistory' element={<ProtectedRoute><Bookinghistory /></ProtectedRoute>}></Route>
-          <Route path="/login" element={<Login/>}></Route>
-        </Routes>
-        <Footer/>
-      </BrowserRouter>
+     <Router>
+  <Navigation />
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/booking" element={<ProtectedRoute><Booking /></ProtectedRoute>} />
+    <Route path="/bookinghistory" element={<ProtectedRoute><Bookinghistory /></ProtectedRoute>} />
+  </Routes>
+  <Footer />
+</Router>
     </>
   )
 }
