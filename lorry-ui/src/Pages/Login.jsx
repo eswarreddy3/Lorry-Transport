6fa4168e80dbe6a61bb6b267c90fd976af67b5c7
Venkata Sai Eswar Reddy Pagadala <109944287+eswarreddy3@users.noreply.github.com>
2025-03-03 12:53:22 +0530

    Update Login.jsx

--- lorry-ui/src/Pages/Login.jsx
+++ lorry-ui/src/Pages/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const response = await fetch('http://localhost:4000/login', {
+    const response = await fetch('https://lorry-backend.onrender.com/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
